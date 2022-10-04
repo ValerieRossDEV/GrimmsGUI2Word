7e28c1af7622a831b131f7c83323e1732afcda9a
Valerie <41258413+ValerieRossDEV@users.noreply.github.com>
2022-10-04 16:19:12 +0300

    fixed GUI

--- src/main/java/valerie/grimms/grimmsgui/GrimmsApplication.java
+++ src/main/java/valerie/grimms/grimmsgui/GrimmsApplication.java
@@ -3,6 +3,9 @@ package valerie.grimms.grimmsgui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +15,10 @@ public class GrimmsApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GrimmsApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 650, 500);
+
         stage.setTitle("Grimm's GUI");
         stage.setScene(scene);
+
         stage.show();
     }
 
